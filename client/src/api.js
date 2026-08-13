795802723ab3bb40ce566157963d7e8842e4e213
Ranjana V R <2403717673722037@cit.edu.in>
2026-08-13 12:45:57 +0530

    remove trailing space in auth backend url

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,5 @@
 // Base URL of the Node/Express + MongoDB auth server (see /server)
-export const AUTH_BACKEND = "https://schemesaathi-auth.onrender.com ";
+export const AUTH_BACKEND = "https://schemesaathi-auth.onrender.com";
 
 export async function apiPost(path, body, token) {
   const res = await fetch(`${AUTH_BACKEND}${path}`, {
